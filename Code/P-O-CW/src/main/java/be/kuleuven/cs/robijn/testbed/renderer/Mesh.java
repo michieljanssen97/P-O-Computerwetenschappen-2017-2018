c6330abee8086ee45ba908136f55caedbd103fd8
Unknown <wouterdekrsmckr@gmail.com>
2017-10-18 13:38:59 +0200

    Implemented rendering function, fixed cube triangulation, improved memory management

--- Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/testbed/renderer/Mesh.java
+++ Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/testbed/renderer/Mesh.java
@@ -7,6 +7,7 @@ import static org.lwjgl.opengl.GL11.GL_FLOAT;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
 import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
 import static org.lwjgl.opengl.GL20.glGetAttribLocation;
 import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 import static org.lwjgl.opengl.GL30.glBindVertexArray;
@@ -37,6 +38,8 @@ public class Mesh implements AutoCloseable {
         }
 
         //Create Vertex Array Object
+        //The buffers and their properties will be stored in this buffer:
+        //See also: https://stackoverflow.com/questions/17149728/when-should-glvertexattribpointer-be-called
         int vertexArrayId = glGenVertexArrays();
         glBindVertexArray(vertexArrayId);
 
@@ -44,7 +47,8 @@ public class Mesh implements AutoCloseable {
         int vertexBufferId = glGenBuffers();
         glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
         glBufferData(GL_ARRAY_BUFFER, vertexPositions, GL_STATIC_DRAW);
-        //glVertexAttribPointer(0,3, GL_FLOAT,false, 0, 0);
+        glVertexAttribPointer(0,3, GL_FLOAT,false, 0, 0);
+        glEnableVertexAttribArray(0);
 
         //Create texture coordinates buffer
         int textureCoordinatesBuffer = -1;
@@ -52,7 +56,8 @@ public class Mesh implements AutoCloseable {
             textureCoordinatesBuffer = glGenBuffers();
             glBindBuffer(GL_ARRAY_BUFFER, textureCoordinatesBuffer);
             glBufferData(textureCoordinatesBuffer, textureCoordinates, GL_STATIC_DRAW);
-            //glVertexAttribPointer(1,2, GL_FLOAT,false, 0, 0);
+            glVertexAttribPointer(1,2, GL_FLOAT,false, 0, 0);
+            glEnableVertexAttribArray(1);
         }
 
         //Create normals buffer
@@ -61,42 +66,57 @@ public class Mesh implements AutoCloseable {
             normalsBufferId = glGenBuffers();
             glBindBuffer(GL_ARRAY_BUFFER, normalsBufferId);
             glBufferData(GL_ARRAY_BUFFER, normals, GL_STATIC_DRAW);
-            //glVertexAttribPointer(2,3, GL_FLOAT,false, 0, 0);
+            glVertexAttribPointer(2,3, GL_FLOAT,false, 0, 0);
+            glEnableVertexAttribArray(2);
         }
 
         //Create indices buffer
         int indicesBufferId = glGenBuffers();
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesBufferId);
-        //glBufferData(indicesBufferId, faceVertexIndices, GL_STATIC_DRAW);
+        glBufferData(indicesBufferId, faceIndices, GL_STATIC_DRAW);
 
-        return new Mesh(vertexArrayId, vertexBufferId, textureCoordinatesBuffer, normalsBufferId, indicesBufferId);
+        //According to section "5.1.2 Automatic Unbinding of Deleted Objects" and 5.1.3 in the OpenGL spec,
+        //you can 'delete' buffers, textures, renderbuffers, ... without actually removing them from GPU memory.
+        //When these objects are references by a VAO which is not currently bound to the context, removing them
+        //will only delete the 'name' but not the actual object. The objects will be deleted when the last reference
+        //to them is removed.
+
+        //Unbind vertex array
+        glBindVertexArray(0);
+
+        //Delete buffers when the vertex array is deleted
+        glDeleteBuffers(vertexBufferId);
+        if(textureCoordinatesBuffer != -1){
+            glDeleteBuffers(textureCoordinatesBuffer);
+        }
+        if(normalsBufferId != -1){
+            glDeleteBuffers(normalsBufferId);
+        }
+        if(indicesBufferId != -1){
+            glDeleteBuffers(indicesBufferId);
+        }
+
+        return new Mesh(vertexArrayId, vertexPositions.length/3);
     }
 
     private int vertexArrayObjectId;
-    private int vertexPositionsBufferId;
-    private int textureCoordinatesBufferId;
-    private int normalsBufferId;
-    private int indicesBufferId;
+    private int vertexCount;
 
-    private Mesh(int vertexArrayObjectId, int vertexPositionsBufferId,
-                 int textureCoordinatesBufferId, int normalsBufferId, int indicesBufferId){
+    private Mesh(int vertexArrayObjectId, int vertexCount){
         this.vertexArrayObjectId = vertexArrayObjectId;
-        this.vertexPositionsBufferId = vertexPositionsBufferId;
-        this.textureCoordinatesBufferId = textureCoordinatesBufferId;
-        this.normalsBufferId = normalsBufferId;
-        this.indicesBufferId = indicesBufferId;
+        this.vertexCount = vertexCount;
+    }
+
+    public int getVertexArrayObjectId(){
+        return vertexArrayObjectId;
+    }
+
+    public int getVertexCount(){
+        return vertexCount;
     }
 
     @Override
     public void close() {
         glDeleteVertexArrays(vertexArrayObjectId);
-        glDeleteBuffers(vertexPositionsBufferId);
-        if(textureCoordinatesBufferId != -1){
-            glDeleteBuffers(textureCoordinatesBufferId);
-        }
-        if(normalsBufferId != -1){
-            glDeleteBuffers(normalsBufferId);
-        }
-        glDeleteBuffers(indicesBufferId);
     }
 }
