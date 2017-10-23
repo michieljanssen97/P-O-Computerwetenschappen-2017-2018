0e6505c5c48879e776bbf27291a3255a339e9957
Unknown <wouterdekrsmckr@gmail.com>
2017-10-23 16:00:17 +0200

    Start on GUI

--- Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/gui/GUI.java
+++ Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/gui/GUI.java
@@ -1,11 +1,22 @@
 package be.kuleuven.cs.robijn.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.load(Resources);
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
 
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
     }
 }
