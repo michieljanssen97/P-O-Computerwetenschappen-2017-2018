f90ffd9a1287dfb747ce7d48c98ad85ba083d13c
Unknown <michiel_janssen@hotmail.com>
2017-11-13 06:50:57 -0800

    Fixed stutter of the camera

---
+++ Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/common/UpdateEventHandler.java
@@ -0,0 +1,39 @@
+package be.kuleuven.cs.robijn.common;
+
+
+import p_en_o_cw_2017.AutopilotInputs;
+import p_en_o_cw_2017.AutopilotOutputs;
+
+import java.util.function.BiConsumer;
+
+
+public class UpdateEventHandler implements Comparable<UpdateEventHandler>{
+
+    public static final int HIGH_PRIORITY = 100;
+    public static final int MEDIUM_PRIORITY = 50;
+    public static final int LOW_PRIORITY = 0;
+
+    private BiConsumer<AutopilotInputs, AutopilotOutputs> function;
+    private int priority;
+
+    public UpdateEventHandler(BiConsumer<AutopilotInputs, AutopilotOutputs> function, int priority){
+
+        this.function = function;
+        this.priority = priority;
+    }
+
+
+    public BiConsumer<AutopilotInputs, AutopilotOutputs> getFunction() {
+        return function;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+
+    @Override
+    public int compareTo(UpdateEventHandler updateEventHandler) {
+        return updateEventHandler.getPriority() - getPriority();
+    }
+}
