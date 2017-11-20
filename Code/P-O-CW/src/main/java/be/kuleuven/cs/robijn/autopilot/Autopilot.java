7123a6803e084f8047910615dc5b665552105e24
pietervandensande <pieter.vandensande@student.kuleuven.be>
2017-11-20 19:04:45 +0100

    -changes to implementation given testbed

--- Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/autopilot/Autopilot.java
+++ Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/autopilot/Autopilot.java
@@ -16,7 +16,6 @@ import interfaces.*;
  * @author Pieter Vandensande
  */
 public class Autopilot extends WorldObject implements interfaces.Autopilot {
-
 	public AutopilotConfig getConfig() {
 		return this.config;
 	}
@@ -56,7 +55,7 @@ public class Autopilot extends WorldObject implements interfaces.Autopilot {
 		if (this.isFirstUpdate())
 			this.firstUpdate = false;
 		else {
-			this.moveDrone(inputs.getElapsedTime() - this.getPreviousElapsedTime(), inputs);
+			this.moveDrone(inputs.getElapsedTime(), inputs);
 		}
         this.setPreviousElapsedTime(inputs.getElapsedTime());  
         
@@ -85,7 +84,7 @@ public class Autopilot extends WorldObject implements interfaces.Autopilot {
 		double absoluteAccuracy = 1.0e-8;
 		int maxOrder = 5;
 		UnivariateSolver solver = new BracketingNthOrderBrentSolver(relativeAccuracy, absoluteAccuracy, maxOrder);
-		float turningTime = 1.0f;
+		float turningTime = 0.5f;
 		
 		//Case 1
 //		float maxInclinationWing = this.minMaxInclination((float)(Math.PI/2), (float)0.0, true, (float)((1.0/360.0)*2*Math.PI), drone, 1);
@@ -749,8 +748,20 @@ public class Autopilot extends WorldObject implements interfaces.Autopilot {
 		
 		RealVector newPosition = new ArrayRealVector(new double[] {inputs.getX(), inputs.getY(), inputs.getZ()}, false);
 		float newHeading = inputs.getHeading();
+		if (newHeading < 0)
+			newHeading += (2*Math.PI);
+		if (newHeading >= 2*Math.PI)
+			newHeading = 0;
 		float newPitch = inputs.getPitch();
+		if (newPitch < 0)
+			newPitch += (2*Math.PI);
+		if (newPitch >= 2*Math.PI)
+			newPitch = 0;
 		float newRoll = inputs.getRoll();
+		if (newRoll < 0)
+			newRoll += (2*Math.PI);
+		if (newRoll >= 2*Math.PI)
+			newRoll = 0;
 		
 		drone.setRelativePosition(newPosition);
 		drone.setHeading(newHeading);
