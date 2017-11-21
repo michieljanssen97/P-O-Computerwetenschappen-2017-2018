cc7ad20cf16c48228058b68a90c6aff75e94300e
Unknown <wouterdekrsmckr@gmail.com>
2017-11-21 12:22:20 +0100

    Implemented icons, enabled opacity blending in rendering pipeline
    Fixed bug in texture loading, enabled transparency loading in textures and shaders.

--- Code/P-O-CW/src/main/resources/shaders/textured/fragment.glsl
+++ Code/P-O-CW/src/main/resources/shaders/textured/fragment.glsl
@@ -2,8 +2,8 @@
 
 uniform sampler2D inputTexture;
 in vec2 fragTextureCoordinate;
-out vec3 outColor;
+out vec4 outColor;
 
 void main() {
-    outColor = texture(inputTexture, fragTextureCoordinate).rgb;
+    outColor = texture(inputTexture, fragTextureCoordinate).rgba;
 }
