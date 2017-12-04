2c6083d7407f984e9901cbfa4faf620f6e92af0f
Unknown <wouterdekrsmckr@gmail.com>
2017-12-04 14:22:35 +0100

    Integrated cube file gui with flight controller

--- Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/testbed/VirtualTestbed.java
+++ Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/testbed/VirtualTestbed.java
@@ -11,6 +11,7 @@ import be.kuleuven.cs.robijn.testbed.renderer.OpenGLRenderer;
 import p_en_o_cw_2017.*;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * A class of virtual testbeds.
@@ -27,7 +28,7 @@ public class VirtualTestbed extends WorldObject implements TestBed {
 	private PerspectiveCamera droneCamera;
 	private byte[] latestCameraImage;
 	
-	private ArrayList<Box> allBoxList = new ArrayList<Box>();
+	private ArrayList<Box> boxesToFlyTo = new ArrayList<>();
 
 	public VirtualTestbed(List<Box> boxes, AutopilotConfig config, RealVector initialVelocity) {
 		this.config = config;
@@ -39,14 +40,7 @@ public class VirtualTestbed extends WorldObject implements TestBed {
 		for (Box box : boxes){
 			this.addChild(box);
 		}
-	}
-	
-	public ArrayList<Box> getAllBoxes(){
-		return allBoxList;
-	}
-	
-	public void removeBoxFromList(Box box) {
-		allBoxList.remove(box);
+		boxesToFlyTo.addAll(boxes);
 	}
 
 	public boolean update(float secondsSinceStart, float secondsSinceLastUpdate, AutopilotOutputs output){
@@ -62,13 +56,13 @@ public class VirtualTestbed extends WorldObject implements TestBed {
 		//true if the center of mass of the drone is in a specified distance of the center of a cube
 		if (calculateDistanceToDrone(box, drone) <= stopDistanceToCenterBox){
 			//The box should not be taken into account anymore, it is already reached
-			removeBoxFromList(box); 
+			boxesToFlyTo.remove(box);
 			
 			//Remove the reached box from the testbed
 			removeChild(box);
 			
 			//Stop the simulation if all boxes are handled
-			if(getAllBoxes().isEmpty()) {
+			if(boxesToFlyTo.isEmpty()) {
 				return true;
 			}
 		}
@@ -84,22 +78,13 @@ public class VirtualTestbed extends WorldObject implements TestBed {
 	 * Get the box closest to the current position of the drone
 	 */
 	public Box getClosestBox(Drone drone) {
-		ArrayList<Box> allBoxes = new ArrayList<>(getAllBoxes());
+		Optional<Box> closestBox = boxesToFlyTo.stream().min((boxA, boxB) -> {
+			double distanceBoxAToDrone = calculateDistanceToDrone(boxA, drone);
+			double distanceBoxBToDrone = calculateDistanceToDrone(boxB, drone);
+			return Double.compare(distanceBoxAToDrone, distanceBoxBToDrone);
+		});
 		
-		Box closestBox = allBoxes.get(0);
-		double distanceClosestBoxToDrone = calculateDistanceToDrone(closestBox, drone);
-		
-		allBoxes.remove(0); //first element is set as default, so should not be checked anymore
-		for (Box b:allBoxes) {
-			double distanceCurrentBoxToDrone = calculateDistanceToDrone(b, drone);
-			if (distanceCurrentBoxToDrone < distanceClosestBoxToDrone) {
-				//set the new box as closest box
-				closestBox = b;
-				distanceClosestBoxToDrone = distanceCurrentBoxToDrone;
-			}
-		}
-		
-		return closestBox;
+		return closestBox.orElse(null);
 	}
 	
 	/**
