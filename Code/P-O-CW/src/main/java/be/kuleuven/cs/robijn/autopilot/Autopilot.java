c563e1b722587477000a5ccde996235ba5e87291
pietervandensande <pieter.vandensande@student.kuleuven.be>
2017-12-04 11:27:15 +0100

    -checked some things and our physics look to be ok (tests show that the problem is with the image recognition)

--- Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/autopilot/Autopilot.java
+++ Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/autopilot/Autopilot.java
@@ -480,6 +480,8 @@ public class Autopilot extends WorldObject implements interfaces.Autopilot {
 			}
         };
         
+        System.out.println("help");
+        System.out.println(drone.getAngularAccelerations(leftWingInclination, rightWingInclination, horStabInclination, verStabInclination)[1]);
         return output;
 	}
 	
@@ -676,6 +678,7 @@ public class Autopilot extends WorldObject implements interfaces.Autopilot {
 		float newRollAngularVelocity = this.getPreviousRollAngularVelocity()
 				+ 2*(newAverageRollAngularVelocity - this.getPreviousRollAngularVelocity());
 		
+		System.out.println((newPitchAngularVelocity - this.getPreviousPitchAngularVelocity())/secondsSinceLastUpdate);
 		drone.setVelocity(newVelocity);
 		drone.setHeadingAngularVelocity(newHeadingAngularVelocity);
 		drone.setPitchAngularVelocity(newPitchAngularVelocity);
