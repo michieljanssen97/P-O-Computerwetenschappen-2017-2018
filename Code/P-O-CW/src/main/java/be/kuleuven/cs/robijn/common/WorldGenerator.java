d9186b72edcf8cc24c23d07f4084030c5486051e
Wouter <wouterdekrsmckr@gmail.com>
2017-12-17 00:12:46 +0100

    Changed world generator box distribution to normal distribution

--- Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/common/WorldGenerator.java
+++ Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/common/WorldGenerator.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.DoubleStream;
 
+import org.apache.commons.math3.distribution.NormalDistribution;
 import org.apache.commons.math3.linear.ArrayRealVector;
 
 public class WorldGenerator {
@@ -13,15 +14,15 @@ public class WorldGenerator {
 	 */
 	public static List<Box> generateBoxes(WorldGeneratorSettings settings) {
 		ArrayList<Box> boxes = new ArrayList<>(settings.getBoxCount());
-		Random rand = new Random();
+        NormalDistribution dist = new NormalDistribution(0, 1d/2.575829303d); //99% in [-1; 1]
 
 		for(int i = 1; i <= settings.getBoxCount(); i++) {
 			Box box = new Box();
 
 			double radius = 10.0; //radius around z-axis in which cubes can be generated
-			double x = ((rand.nextDouble() * 2.0) - 1.0) * radius;
+			double x = getBoundedMultiplier(dist) * radius;
 			double maxY = Math.sqrt(Math.pow(radius, 2) - Math.pow(x, 2));
-			double y = ((rand.nextDouble() * 2.0) - 1.0) * maxY;
+			double y = getBoundedMultiplier(dist) * maxY;
 			double z = i * -40; //given in the assignment
 
 			box.setRelativePosition(new ArrayRealVector(new double[] {x, y, z}, false));
@@ -32,6 +33,16 @@ public class WorldGenerator {
 		}
 		return boxes;
 	}
+
+    /**
+     * Returns a sample from the provided normal distribution, guaranteed to have an absolute value less than 1.
+     * This makes the normal distribution slightly inaccurate. (depending on % of values outside of [-1;1])
+     */
+	private static double getBoundedMultiplier(NormalDistribution dist){
+	    double factor;
+	    while(Math.abs(factor = dist.sample()) > 1); //99% chance of false
+	    return factor;
+    }
 
 	public static class WorldGeneratorSettings {
 		private int boxCount = 5;
