bb3123f09936de979757e7f3061d54b22c4d5b62
WoutMees <wout.mees@student.kuleuven.be>
2018-05-08 16:39:00 +0200

    Further fix of tarmac detection

--- Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/worldObjects/GroundPlane.java
+++ Code/P-O-CW/src/main/java/be/kuleuven/cs/robijn/worldObjects/GroundPlane.java
@@ -22,13 +22,26 @@ public class GroundPlane extends WorldObject{
 	public boolean isGrass(double groundPosX, double groundPosZ) {
 		for (Airport air : this.airports) {
 			RealVector airportPos = air.getWorldPosition();
+			Vector2D airport2DPos = new Vector2D(airportPos.getEntry(0), airportPos.getEntry(2));
 			Vector2D airportSize = air.getSize();
+			double angle = air.getAngle()+Math.PI;
+			Vector2D tyrePos = new Vector2D(groundPosX, groundPosZ);
 			
-			//TODO fix if condition, depends on rotation
-			if ( (groundPosX >= airportPos.getEntry(0)-(airportSize.getX()/2)) && 
-					(groundPosX <= airportPos.getEntry(0)+(airportSize.getX()/2)) && 
-					(groundPosZ >= airportPos.getEntry(2)-(airportSize.getY()/2)) && 
-					(groundPosZ <= airportPos.getEntry(2)+(airportSize.getY()/2)) )
+			Vector2D up = new Vector2D(airportSize.getX()/2, new Vector2D(-Math.cos(angle), -Math.sin(angle)));		
+			Vector2D down = new Vector2D(airportSize.getX()/2, new Vector2D(Math.cos(angle), Math.sin(angle)));		
+			Vector2D right = new Vector2D(airportSize.getY()/2, new Vector2D(-Math.sin(angle), Math.cos(angle)));		
+			Vector2D left = new Vector2D(airportSize.getY()/2, new Vector2D(Math.sin(angle), -Math.cos(angle)));
+			
+			Vector2D leftUpCorner = airport2DPos.add(up).add(right);
+			Vector2D leftDownCorner = airport2DPos.add(up).add(left);
+			Vector2D rightDownCorner = airport2DPos.add(down).add(left);
+			Vector2D rightUpCorner = airport2DPos.add(down).add(right);
+			
+			//The rectangle consists out of four vectors. If the tyre position is left of all four vectors, it's located within the rectangle's area.
+			if ( Math.atan2(leftDownCorner.getY()-leftUpCorner.getY(), leftDownCorner.getX() - leftUpCorner.getX()) > Math.atan2(tyrePos.getY()-leftUpCorner.getY(), tyrePos.getX()-leftUpCorner.getX()) &&
+					Math.atan2(rightDownCorner.getY()-leftDownCorner.getY(), rightDownCorner.getX() - leftDownCorner.getX()) > Math.atan2(tyrePos.getY()-leftDownCorner.getY(), tyrePos.getX()-leftDownCorner.getX()) &&
+					Math.atan2(rightUpCorner.getY()-rightDownCorner.getY(), rightUpCorner.getX() - rightDownCorner.getX()) > Math.atan2(tyrePos.getY()-rightDownCorner.getY(), tyrePos.getX()-rightDownCorner.getX()) &&
+					Math.atan2(leftUpCorner.getY()-rightUpCorner.getY(), leftUpCorner.getX() - rightUpCorner.getX()) > Math.atan2(tyrePos.getY()-rightUpCorner.getY(), tyrePos.getX()-rightUpCorner.getX()) )
 				return false;
 		}
 		return true;
